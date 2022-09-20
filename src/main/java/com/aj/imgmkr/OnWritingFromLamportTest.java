a3e5bb2c81c034c8a5a787dc42bfd105a5973380
saucemaker <jana.poornavel@askjuno.com>
2022-09-20 19:53:49 +0530

    add on writing

---
+++ src/main/java/com/aj/imgmkr/OnWritingFromLamportTest.java
@@ -0,0 +1,109 @@
+package com.aj.imgmkr;
+
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.font.GlyphVector;
+import java.awt.font.TextAttribute;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.AttributedString;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+import ij.ImagePlus;
+
+//That’s how people learn — from meeting real people through one-on-one, one-on-three, one-on-four, and one-on-more.
+public class OnWritingFromLamportTest {
+
+	@Test
+	public void createImage() throws IOException {
+
+		String imagePath = this.getClass().getResource("512px-Hinweiszeichen_17a_empty.jpg").getPath();
+		String text1 = "Leslie lamport quoting guindon--";
+		String text2 = "Writing is nature's way of letting ";
+		String text3 = "you know how sloppy your ";
+		String text4 = "thinkingis... Write often, do it...";
+
+		List<String> textList = new ArrayList<>();
+		textList.add(text1);
+		textList.add(text2);
+		textList.add(text3);
+		textList.add(text4);
+
+		File sourceFile = new File(imagePath);
+		BufferedImage image = ImageIO.read(sourceFile);
+		Graphics g = image.getGraphics();
+		Font font = createFontToFit(new Font("Ubuntu", Font.BOLD, 80), text1, image);
+		FontMetrics metrics = g.getFontMetrics(font);
+
+		int x = getX(image, text1, metrics);
+		int y = getY(image, metrics) - 60;
+		for (Iterator<String> iterator = textList.iterator(); iterator.hasNext();) {
+			String text = (String) iterator.next();
+			AttributedString attributedText1 = createAttributedText(text, font);
+			g.drawString(attributedText1.getIterator(), x, y);
+			y = y + 35;
+		}
+
+		ImagePlus resultGraphicsCentered = new ImagePlus("", image);
+		resultGraphicsCentered.show();
+		String nextFile = "/home/jpvel/" + "output.jpg";
+		File nextFilePath = new File(nextFile);
+		ImageIO.write(image, "jpg", nextFilePath);
+
+		System.out.println("Hold on");
+	}
+
+	private int getY(BufferedImage image, FontMetrics metrics) {
+		int positionY = (image.getHeight() - metrics.getHeight()) / 2 + metrics.getAscent();
+		return positionY;
+	}
+
+	private int getX(BufferedImage image, String text1, FontMetrics metrics) {
+		int positionX = (image.getWidth() - metrics.stringWidth(text1)) / 2;
+		return positionX;
+	}
+
+	private AttributedString createAttributedText(String text1, Font font) {
+		AttributedString attributedText1 = new AttributedString(text1);
+		attributedText1.addAttribute(TextAttribute.FONT, font);
+		attributedText1.addAttribute(TextAttribute.FOREGROUND, Color.BLACK);
+		return attributedText1;
+	}
+
+	public static Font createFontToFit(Font baseFont, String text, BufferedImage image) throws IOException {
+		Font newFont = baseFont;
+
+		FontMetrics ruler = image.getGraphics().getFontMetrics(baseFont);
+		GlyphVector vector = baseFont.createGlyphVector(ruler.getFontRenderContext(), text);
+
+		Shape outline = vector.getOutline(0, 0);
+
+		double expectedWidth = outline.getBounds().getWidth();
+		double expectedHeight = outline.getBounds().getHeight();
+
+		boolean textFits = image.getWidth() >= expectedWidth && image.getHeight() >= expectedHeight;
+
+		if (!textFits) {
+			double widthBasedFontSize = (baseFont.getSize2D() * image.getWidth()) / expectedWidth;
+			double heightBasedFontSize = (baseFont.getSize2D() * image.getHeight()) / expectedHeight;
+
+			double newFontSize = widthBasedFontSize < heightBasedFontSize ? widthBasedFontSize : heightBasedFontSize;
+			newFontSize = newFontSize - 10;
+			newFont = baseFont.deriveFont(baseFont.getStyle(), (float) newFontSize);
+		}
+		return newFont;
+	}
+
+}
